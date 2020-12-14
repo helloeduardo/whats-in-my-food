131b344a20cb5e4326edb623df7f4669e02b5d6c
Eduardo Parra <helloeduardo@icloud.com>
2020-12-14 10:10:22 -0700

    Add service spec

--- spec/services/foods_service_spec.rb
+++ spec/services/foods_service_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 describe FoodsService do
   describe "class methods" do
-    describe "#foods_containing(keyword)" do
+    it "#foods_containing(keyword)" do
       results = FoodsService.foods_containing("sweet%20potatoes")
-      require "pry"; binding.pry
+
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:totalHits)
+      expect(results).to have_key(:foods)
+
+      expect(results[:foods]).to be_a(Array)
+      expect(results[:foods].count).to eq(10)
+
+      expect(results[:foods].first).to have_key(:description)
+      expect(results[:foods].first).to have_key(:gtinUpc)
+      expect(results[:foods].first).to have_key(:brandOwner)
+      expect(results[:foods].first).to have_key(:ingredients)
     end
   end
 end
