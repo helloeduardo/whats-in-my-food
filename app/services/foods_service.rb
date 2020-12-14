c6082ce679ad6442591678ead08aa315073dfb8b
Eduardo Parra <helloeduardo@icloud.com>
2020-12-14 10:10:33 -0700

    Add service

--- app/services/foods_service.rb
+++ app/services/foods_service.rb
@@ -1,16 +1,22 @@
 class FoodsService
   def self.foods_containing(keyword)
-    get_json("/v1/foods/search?query=#{keyword}")
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['FDC_API_KEY']}&query=#{keyword}&pageSize=10")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.conn
-   Faraday.new(url: 'https://api.nal.usda.gov/fdc') do |faraday|
-     faraday.headers['x-api-key'] = ENV['FDC_API_KEY']
-   end
- end
-
-   def self.get_json(url)
-     response = conn.get(url)
-     JSON.parse(response.body, symbolize_names: true)
-   end
+  # def self.foods_containing(keyword)
+  #   get_json("/v1/foods/search?query=#{keyword}")
+  # end
+  #
+  # def self.conn
+  #   Faraday.new(url: 'https://api.nal.usda.gov/fdc') do |faraday|
+  #     faraday.params['api_key'] = ENV['FDC_API_KEY']
+  #     faraday.params['pageSize'] = "10"
+  #   end
+  # end
+  #
+  # def self.get_json(url)
+  #   response = conn.get(url)
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
 end
